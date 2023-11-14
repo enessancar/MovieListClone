42bd18c43071a27cdaf53233352d8296329af037
enessancar <enes57751907@gmail.com>
2023-11-14 11:04:49 +0300

    I created home view horizontal collection vie

---
+++ MovieList/Scenes/MainPage/Cell/HorizontalCell/HorizontalCollectionalCell.swift
@@ -0,0 +1,17 @@
+//
+//  HorizontalCollectionalCell.swift
+//  MovieList
+//
+//  Created by Enes Sancar on 14.11.2023.
+//
+
+import UIKit
+
+final class HorizontalCollectionalCell: UICollectionViewCell {
+    
+    //MARK: - Properties
+    @IBOutlet weak var cellImageView: UIImageView!
+    @IBOutlet weak var titleLabel: UILabel!
+    @IBOutlet weak var subtitleLabel: UILabel!
+    
+}
